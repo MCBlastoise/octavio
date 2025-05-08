10d5869cd111cf0e8f77b5ed528cd35b1e7a6232
Ayyub Abdulrezak <ayyubabdul01@gmail.com>
2025-05-08 16:52:26 -0400

    Adding Pi setup scripting

---
+++ setup/setup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Establish device information
+
+# Create WIFI connections
+
+# Generate SSH keys on Pi
+
+# Key-exchange with server
+
+# Setup tunnel-to-lab systemd service
+
+# Make and populate venv environment
+
+# Do separate installs (e.g. audio stuff) necessary
+
+# Construct project-specific files
+
+# Create (copy over) and activate client systemd service
