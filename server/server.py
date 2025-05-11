47665831a87650725f4454155727766ae6886510
Ayyub Abdulrezak <ayyubabdul01@gmail.com>
2025-05-10 23:55:34 -0400

    Restructured project

--- server/server.py
+++ server/server.py
@@ -1,3 +1,7 @@
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+if project_root not in sys.path:
+    sys.path.insert(0, project_root)
+
 from flask import Flask, request
 import numpy as np
 import pyaudio
