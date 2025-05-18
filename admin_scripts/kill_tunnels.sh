f42b1d5b6c38da221318d4caa6ea82e0ea867c07
Ayyub Abdulrezak <ayyubabdul01@gmail.com>
2025-05-17 21:04:54 -0400

    Fixed kill

--- admin_scripts/kill_tunnels.sh
+++ admin_scripts/kill_tunnels.sh
@@ -3,5 +3,5 @@
 MAX_DEVICE_NUM=20
 for i in $(seq 1 "$MAX_DEVICE_NUM"); do
     export TUNNEL_PORT=$((i + 2000))
-    sudo -E lsof -i :${TUNNEL_PORT} | awk 'NR>1 {print $2; exit}' | sort -u | xargs -r -I{} sh -c 'echo "Killing PID {} associated with port ${TUNNEL_PORT}; sudo kill {}"'
+    sudo -E lsof -i :${TUNNEL_PORT} | awk 'NR>1 {print $2; exit}' | sort -u | xargs -r -I{} sh -c 'echo "Killing PID {} associated with port ${TUNNEL_PORT}"; sudo kill {}'
 done
