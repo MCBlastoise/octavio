ac7ba8fc44ed7727d36fd2d7160234f1af9c0e27
Ayyub Abdulrezak <ayyubabdul01@gmail.com>
2025-05-17 22:59:18 -0400

    Stopped prematurely starting client service

--- setup/setup_installation.sh
+++ setup/setup_installation.sh
@@ -55,7 +55,6 @@ CLIENT_SERVICE_NAME="octavio"
 sudo -E envsubst < "$OCTAVIO_PROJECT_PATH/setup/client_template.txt" > /etc/systemd/system/$CLIENT_SERVICE_NAME.service
 sudo systemctl daemon-reload
 sudo systemctl enable $CLIENT_SERVICE_NAME.service
-sudo systemctl start $CLIENT_SERVICE_NAME.service
 
 echo
 
